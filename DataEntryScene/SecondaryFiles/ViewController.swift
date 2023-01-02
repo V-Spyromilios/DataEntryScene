66cf09b606dfbb663fc2649552e46bfb993255b1
Evangelos Spyromilios <71439594+V-Spyromilios@users.noreply.github.com>
2023-01-02 12:19:50 +0000

    setLabels

--- DataEntryScene/SecondaryFiles/ViewController.swift
+++ DataEntryScene/SecondaryFiles/ViewController.swift
@@ -10,32 +10,129 @@ import UIKit
 class ViewController: UIViewController {
 	@IBOutlet var titleView: UITextView!
 	@IBOutlet var descriptionView: UITextView!
-	
 	@IBOutlet var cgiImage: UIImageView!
-	
+
+	@IBOutlet weak var titleLable: UILabel!
+
+	@IBOutlet weak var descriptionLabel: UILabel!
 	override func viewDidLoad() {
 		setTitleView()
 		setDescriptionView()
 		setImageView()
 	}
 	
+	
+	
 	func setTitleView() {
 		titleView.textContainer.maximumNumberOfLines = 1
 		titleView.isScrollEnabled = false
 		titleView.textContainer.lineBreakMode = .byTruncatingTail
+		setPlaceholder(withText: "Title...", label: titleLable, view: titleView)
+		
 	}
 	
 	func setDescriptionView() {
 		descriptionView.textContainer.maximumNumberOfLines = 3
 		descriptionView.isScrollEnabled = false
 		descriptionView.textContainer.lineBreakMode = .byTruncatingTail
+		setPlaceholder(withText: "Description...", label: descriptionLabel, view: descriptionView)
+		
 	}
 	
 	func setImageView() {
-		cgiImage.isUserInteractionEnabled = false
-		cgiImage.layer.borderColor = CGColor(red: 0, green: 0, blue: 0, alpha: 0.8)
-		cgiImage.layer.borderWidth = 2.0
-		
+		cgiImage.isUserInteractionEnabled 	= false
+		cgiImage.layer.borderColor 	= CGColor(red: 0, green: 0, blue: 0, alpha: 0.8)
+		cgiImage.layer.borderWidth 	= 2.0
 	}
+	
+	func setPlaceholder(withText: String, label: UILabel, view: UITextView) {
+		label.text = withText
+		label.font = UIFont.boldSystemFont(ofSize: 13.0)
+		label.sizeToFit()
+		label.textColor = .white
+		label.isHidden = !view.text.isEmpty
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+//	func addPlaceholder() {
+//		var placeholder = UILabel()
+//		placeholder.text = "Add Description..."
+//		placeholder.font = UIFont.init(name: "Lato-Regular", size: 14.0) ?? UIFont.boldSystemFont(ofSize: 13.0)
+//		placeholder.sizeToFit()
+//		descriptionView.addSubview(placeholder)
+//		placeholder.frame.origin = CGPoint(x: 5, y: (descriptionView.font?.pointSize)! / 2)
+//		placeholder.textColor = UIColor.white
+//		placeholder.isHidden = !descriptionView.text.isEmpty
+//	}
+
+	
+//	func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+//		// get the current text, or use an empty string if that failed
+//		let currentText = textView.text ?? ""
+//
+//		// attempt to read the range they are trying to change, or exit if we can't
+//		guard let stringRange = Range(range, in: currentText) else { return false }
+//
+//		// add their new text to the existing text
+//		let updatedText = currentText.replacingCharacters(in: stringRange, with: text)
+//
+//		// poplast() ?
+//		// make sure the result is under 16 characters
+//		if (textView.contentSize.height / textView.font!.lineHeight) > 3 {
+//			return true
+//		}
+//		return false
+//	}
+
 }
 
+//func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+//	// get the current text, or use an empty string if that failed
+//	let currentText = textView.text ?? ""
+//
+//	// attempt to read the range they are trying to change, or exit if we can't
+//	guard let stringRange = Range(range, in: currentText) else { return false }
+//
+//	// add their new text to the existing text
+//	let updatedText = currentText.replacingCharacters(in: stringRange, with: text)
+//
+//	// make sure the result is under 16 characters
+//	return (textView.contentSize.height / textView.font!.lineHeight) > CGFloat(3)
+//}
+
+
+//extension UITextView {
+//	func checkMaxNumberOfLines(view: UITextView, maxLines: Int) {
+//		if (view.contentSize.height / view.font!.lineHeight) > CGFloat(maxLines) {
+//			view.text.popLast()
+//		}
+//	}
+//
+//	func checkMaxNumberOfLines2(maxLines: Int) {
+//		if (self.contentSize.height / self.font!.lineHeight) > CGFloat(maxLines) {
+//			self.text.popLast()
+//		}
+//	}
+//}
+
+
+//extension UITextViewDelegate {
+//	optional func textViewDidChange(_ textView: UITextView) {
+//		placeholder.isHidden = !textView.text.isEmpty
+//	}
+//}
